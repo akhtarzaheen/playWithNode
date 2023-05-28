ffb31bc8066bfcd9c642301f37ba7b5c94eb676d
akhtarzaheen <akhtarzaheen335@gmail.com>
2023-05-28 22:01:51 +0530

    after read text file and add input text inside it

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(
-    "url=>",
-    req.url,
-    "method=>",
-    req.method,
-    "header=>",
-    req.headers
-  );
   if (req.url === "/") {
     res.write("<html>");
     res.write(
-      "<body><form action='/message' method='post'><input type='text'/><button>Send</button></form></body>"
+      "<body><form action='/message' method='post'><input type='text' name='message'/><button>Send</button></form></body>"
     );
     res.write("</html>");
     return res.end();
   }
   if (req.url === "/message" && req.method === "POST") {
-    fs.writeFileSync("message.txt", "Dummy");
+    // fs.writeFileSync("message.txt", "Dummy");
+
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      fs.writeFileSync("message.txt", parseBody.split("=")[1]);
+    });
     res.setHeader("Location", "/");
     res.statusCode = 302;
     return res.end();
