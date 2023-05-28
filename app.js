25bd5a27c09b29d8ad2d6158a2fc1cfc17cf0e94
akhtarzaheen <akhtarzaheen335@gmail.com>
2023-05-28 15:22:20 +0530

    after get first response from server

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("req=>", req, "res=>", res);
+  console.log(
+    "url=>",
+    req.url,
+    "method=>",
+    req.method,
+    "header=>",
+    req.headers
+  );
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first Node Project!</title></head>");
+  res.write("<body><h1>First Node Project Html!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(3001);
+server.listen(3002);
