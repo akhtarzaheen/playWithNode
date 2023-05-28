1ed69d31922a7f423d98ad2236dfae5115669765
akhtarzaheen <akhtarzaheen335@gmail.com>
2023-05-28 21:05:06 +0530

    after added input text

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(
@@ -9,6 +10,20 @@ const server = http.createServer((req, res) => {
     "header=>",
     req.headers
   );
+  if (req.url === "/") {
+    res.write("<html>");
+    res.write(
+      "<body><form action='/message' method='post'><input type='text'/><button>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (req.url === "/message" && req.method === "POST") {
+    fs.writeFileSync("message.txt", "Dummy");
+    res.setHeader("Location", "/");
+    res.statusCode = 302;
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first Node Project!</title></head>");
