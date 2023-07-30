ab01c47cbd67b5c04b03823b4548996e8b8f0fde
akhtarzaheen <akhtarzaheen335@gmail.com>
2023-07-30 22:19:56 +0530

    added sequelize in app

--- models/product.js
+++ models/product.js
@@ -1,22 +1,41 @@
-const db = require("../util/database");
+const sequelize = require("../util/database");
 
-let products = [];
+const Sequelize = require("sequelize");
 
-// exports.addProduct = (title) => {
-//   products.push({ product: title });
+// exports.saveProducts = ({ title, price, description, imageUrl }) => {
+//   return db.execute(
+//     "INSERT INTO products ( title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
+//     [title, price, description, imageUrl]
+//   );
 // };
 
-exports.saveProducts = ({ title, price, description, imageUrl }) => {
-  return db.execute(
-    "INSERT INTO products ( title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
-    [title, price, description, imageUrl]
-  );
-};
+// exports.fetchall = () => {
+//   return db.execute("select *from products");
+// };
 
-exports.fetchall = () => {
-  return db.execute("select *from products");
-};
+// exports.getProductById = (id) => {
+//   return db.execute("SELECT * FROM products WHERE id=?", [id]);
+// };
 
-exports.getProductById = (id) => {
-  return db.execute("SELECT * FROM products WHERE id=?", [id]);
-};
+const Products = sequelize.define("products", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Products;
